4d26919781faa534ee30d82c8f96b2d423c96fd6
TotoroDavid <davidtoro65@hotmail.com>
2021-03-30 09:05:28 -0500

    added the remove item

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,31 @@
-import { DECRESE, INCREASE, CLEAR_CART} from './actions'
+import { DECRESE, INCREASE, CLEAR_CART, REMOVE} from './actions'
 
 function reducer ( state, action ) {
+
     if (action.type === CLEAR_CART) {
         return {...state,cart:[]}
+    }
+    if (action.type === DECRESE) {
+        console.log('you decreased amount')
+    }
+    if (action.type === INCREASE) {
+        console.log('you increased amount')
+    }
+    if (action.type === REMOVE) {
+        return {...state, cart:state.cart.filter((cartItem) => 
+            cartItem.id !== action.payload.id)}
     }
     return state
 }
 
 export default reducer;
+
+
+
+
+// switch ( action.type ) {
+    //     case CLEAR_CART:
+    //         return {...state,cart:[]}
+    //     default:
+    //         return state
+    // }
